0f075d041706659c700e62fe59917987b8b831c7
Coby Loo Choon Boon <looboon@outlook.com>
2024-08-04 01:05:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,16 +24,13 @@ ingredients_list = st.multiselect(
     my_dataframe,
     max_selections=5,
 )
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-f_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-    # st.write(ingredients_string)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        f_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = """
         insert into smoothies.public.orders(ingredients, name_on_order)
